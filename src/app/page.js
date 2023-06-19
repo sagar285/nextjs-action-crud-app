84dc9b672569cffbe5e577bc2957f8e3a9e9866c
sagar <anandguptasir@gmail.com>
2023-06-20 02:48:13 +0530

    Initial commit from Create Next App

---
+++ src/app/page.js
@@ -0,0 +1,27 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+  <>
+  <main>
+    <h1>Add task</h1>
+    <form>
+      <div>
+        <label htmlFor='title'>
+          title
+        </label>
+        <br/>
+      <input/>
+      </div>
+      <div>
+        <label>
+          title
+        </label>
+        <br/>
+      <input/>
+      </div>
+    </form>
+  </main>
+  </>
+  )
+}
